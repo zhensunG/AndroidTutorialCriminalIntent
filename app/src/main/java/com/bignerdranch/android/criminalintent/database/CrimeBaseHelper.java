5bc48b9e5cc81a69cfa25bf2e2fed8beb4d54337
Zhen Sun <kibazhensun@gmail.com>
2016-11-13 02:40:48 -0800

    Commit 9: Chapter 15 Finished
    Added 2 buttons in CrimeFragment.java with implicit intents.
    Added a new field suspect in Crime class.

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeBaseHelper.java
@@ -25,7 +25,10 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
                 CrimeTable.Cols.UUID + ", " +
                 CrimeTable.Cols.TITLE + ", " +
                 CrimeTable.Cols.DATE + ", " +
-                CrimeTable.Cols.SOLVED + ")");
+                CrimeTable.Cols.SOLVED + ", " +
+                CrimeTable.Cols.SUSPECT +
+                ")"
+        );
     }
 
     @Override
